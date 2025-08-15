1e5f754530642c74f056ec423ccd47212a8606ae
richsi <rzhsieh@gmail.com>
2025-08-15 11:49:36 -0700

    tensor

--- minitorch/tensor.py
+++ minitorch/tensor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from abc import abstractmethod
 from typing import Optional, Tuple, Union
 
 class Tensor:
@@ -6,33 +7,57 @@ class Tensor:
     A Tensor class that wraps a numpy array and supports automatic differentiation
     """
 
-    def __init__(
-        self, data: Union[np.ndarray, list, int, float], requires_grad: bool = False, dtype=np.float32
-    ):
+    def __init__(self, data, _children=(), _op=''):
+        self.data = np.asarray(data)
+        self.grad = np.zeros_like(self.data, dtype=np.float32)
 
-        if isinstance(data, np.ndarray):
-            self.data = data.astype(dtype) 
-        else:
-            self.data = np.array(data, dtype=dtype)
+        # graph building
+        self._backward = lambda: None # stores function call to compute gradients
+        self._prev = set(_children) 
+        self._op = _op # operation that created this Tensor
 
-        self.requires_grad = requires_grad
+    def __repr__(self):
+        return f"Tensor(data={self.data}, grad={self.grad}"
 
-    @property
-    def shape(self) -> Tuple[int, ...]:
-        return self.data.shape
+    def __add__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
 
-    @property 
-    def dtype(self):
-        return self.data.dtype
+        out = Tensor(self.data + other.data, (self, other), '+')
 
-    def __repr__(self) -> str:
-        return f"tensor({self.data}, requires_grad={self.requires_grad}, dtype={self.dtype})"
+        # defining backwards function for +
+        def _backward():
+            self.grad += out.grad 
+            other.grad += out.grad 
+        
+        out._backward = _backward
+        return out
 
-    def zero_grad(self):
-        self.grad = None
+    def __mul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+
+        out = Tensor(self.data * other.data, (self, other), '*')
+
+        # defining backwards function for +
+        def _backward():
+            self.grad += other.data * out.grad 
+            other.grad += self.data * out.grad 
+        
+        out._backward = _backward
+        return out
 
     def backward(self):
-        if not self.requires_grad:
-            raise RuntimeError("Cannot call backward on a tensor that does not require gradients.")
+        topo = []
+        visited = set()
 
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
         
+        build_topo(self)
+        self.grad = np.ones_like(self.data, dtype=np.float32)
+
+        for node in reversed(topo):
+            node._backward()
